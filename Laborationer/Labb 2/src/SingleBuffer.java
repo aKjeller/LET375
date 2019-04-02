2a63cafc9a63e2d8b36c6e84f282cebb49265f9b
aKjeller <44802724+aKjeller@users.noreply.github.com>
2019-04-02 15:49:28 +0200

    Snyggade till

--- Laborationer/Labb 2/src/SingleBuffer.java
+++ Laborationer/Labb 2/src/SingleBuffer.java
@@ -1,24 +1,17 @@
 public class SingleBuffer <T> {
-
 	private T buffer; 
 	
-	
-	
 	@SuppressWarnings("hiding")
-	public T get() {
-		
-		
+	public T get() {		
 		if(buffer==null)
-			return null;
-		
+			return null;	
 		else {
 			T obj = buffer; 
 			buffer=null;
 			return obj; 
-		}
-		
-		
+		}		
 	}
+	
 	public boolean put(T t) {
 		if(buffer==null) {
 			buffer = t;
@@ -27,9 +20,6 @@ public class SingleBuffer <T> {
 		else {
 			return false; 
 		}
-		
-		
-   
-	}
-
+  	}
+	
 }
