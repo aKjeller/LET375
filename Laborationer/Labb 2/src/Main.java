4907b9dac046016f41cb7cbd505b44beb2f5ccf6
aKjeller <44802724+aKjeller@users.noreply.github.com>
2019-04-08 14:26:55 +0200

    Super Mega Super Done!!

--- Laborationer/Labb 2/src/Main.java
+++ Laborationer/Labb 2/src/Main.java
@@ -90,11 +90,10 @@ public class Main {
          p1.add(new Person("Beda","beda@fishnet.cod","female",102));
         
          // Assignment 6: Write code using lambdas here
-         ArrayList<String> p2 = new ArrayList<>();
-         map(x ->{ if(x.getAge()>= 65 && x.getGender() == "female") return p2.add(x.getEmail()); else return null;}, p1);
+         Collection<Person> p2 = filter(x -> x.getGender().equals("female") && x.getAge() >= 65, p1);
+         Collection<String> p3 = map(x -> x.getEmail(), p2);
          System.out.println();
-         print(p2);
-         
+         print(p3);         
     }
 }
 
