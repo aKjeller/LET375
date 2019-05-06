1141b4f2b6f7f49e658ce881b06521ed4269f844
Geddsson <markus.isgr@gmail.com>
2019-05-06 18:51:01 +0200

    AlfredÄrGay

---
+++ Laborationer/Labb 5/src/fraktaler/Penta.java
@@ -0,0 +1,39 @@
+package fraktaler;
+
+public class Penta extends Flake {
+
+	public void draw(Turtle turtle,int n,double size) {
+		this.turtle = turtle;
+		turtle.turnTo(0.0);
+		// Draw a triangular structure of three fractals
+		drawSide(n,size);
+		turtle.turn(-72.0);
+		drawSide(n,size);
+		turtle.turn(-72.0);
+		drawSide(n,size);	
+		turtle.turn(-72.0);
+		drawSide(n,size);
+		turtle.turn(-72.0);
+		drawSide(n,size);
+		turtle.turn(-72);
+	}
+
+	private void drawSide(int n,double size) {
+		if ( n <= 0 )
+			turtle.walk(size);
+		else {
+			double l = size/3.0;
+			drawSide(n-1,l);
+			turtle.turn(-108);
+			drawSide(n-1,l);
+			turtle.turn(72);
+			drawSide(n-1,l);
+			turtle.turn(72);
+			drawSide(n-1,l);
+			turtle.turn(72);
+			drawSide(n-1,l);
+			turtle.turn(-108);
+			drawSide(n-1,l);
+		}
+	}
+}
