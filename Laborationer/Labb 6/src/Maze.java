539eff5ef58cf951efaf452915de1235d6cc4872
Geddsson <markus.isgr@gmail.com>
2019-05-13 14:25:30 +0200

    Labb 6

---
+++ Laborationer/Labb 6/src/Maze.java
@@ -0,0 +1,17 @@
+public class Maze extends Board {
+  		
+    public Maze( int rows, int cols ) {
+    	super(rows,cols);
+//    	 Implement this!
+    }
+    
+    public void create() {
+//    	 Implement this method!
+    }
+    
+    public void search() {
+//    	 Implement this method!
+    }
+    
+//    ...
+}
