273e668e6ae03bdbe3081f400eea09a2ba9235e4
aKjeller <44802724+aKjeller@users.noreply.github.com>
2019-05-27 20:44:51 +0200

    upg7  done

--- Gamla tentor/t_180831/src/uppgift7/Main.java
+++ Gamla tentor/t_180831/src/uppgift7/Main.java
@@ -1,10 +1,41 @@
 package uppgift7;
 
+import java.util.LinkedList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TreeNode t = new TreeNode();
+		t.element = 'A';
+		t.left = new TreeNode();
+		t.left.element = 'B';
+		t.left.left = new TreeNode();
+		t.left.left.element = 'D';
+		
+		t.right = new TreeNode();
+		t.right.element = 'C';
+		t.right.left = new TreeNode();
+		t.right.left.element = 'E';
+		t.right.right = new TreeNode();
+		t.right.right.element = 'F';
+		
+		printLevelOrder(t);
 	}
 
+	public static void printLevelOrder(TreeNode t) {
+		if(t == null)
+			return;
+		else {
+			LinkedList<TreeNode> list = new LinkedList<TreeNode>();
+			list.add(t);
+			while(!list.isEmpty()) {
+				TreeNode next = list.remove();
+				System.out.print(next.element + " ");
+				if(next.left != null)
+					list.add(next.left);
+				if(next.right != null)
+					list.add(next.right);
+			}
+		}		
+	}
 }
