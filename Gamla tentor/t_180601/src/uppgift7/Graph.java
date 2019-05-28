e24e80eac7471fa5c14ffba56afc17c20cdc94ac
aKjeller <44802724+aKjeller@users.noreply.github.com>
2019-05-28 15:54:26 +0200

    Update Graph.java

--- Gamla tentor/t_180601/src/uppgift7/Graph.java
+++ Gamla tentor/t_180601/src/uppgift7/Graph.java
@@ -26,49 +26,9 @@ public class Graph {
 			return;
 		}
 		else {
-			LinkedList<LinkedList<Vertex>> list = new LinkedList<LinkedList<Vertex>>();
-			LinkedList<Vertex> temp = new LinkedList<Vertex>();
-			temp.add(graphTable.get(startVertex));
-			list.add(temp);
-			
-			int level = 0;
-			while(!list.isEmpty()) {
-				System.out.print(level + ": ");
-				
-				for(Vertex str: list.remove()) {
-					System.out.print(str + " ");	
-					list.add((LinkedList)graphTable.get(str.name).neighbours);
-				}
-				
-					
 
-				level++;
-				System.out.println();
-
-				
-			}
 		}
 	}
-	public void levelOrder(String startVertex) {     
-		Queue<Vertex> q = new LinkedList<>();     
-		q.add(graphTable.get(startVertex));     
-		int dist = 0;     System.out.print(dist + ": ");     
-		while ( ! q.isEmpty() ) { 
-		        Vertex v = q.poll();        
-		        if ( ! v.visited ) {            
-		        	if ( v.dist == dist + 1 ) { // Next level                 
-		        		dist++;                 
-		        		System.out.print("\n" + dist + ": ");             
-		        	}             
-		        	System.out.print(v + " ");             
-		        	v.visited = true;             
-		        	for ( Vertex w : v.neighbours ) {                 
-		        		if ( w.dist == 0 )                     
-		        		w.dist = v.dist + 1;                
-		        		q.add(w);            
-		        	}    	     
-		        }     
-		} 	
-	}
+
 	
 }
